550083cc07eab095398ad8806b7282261ec75041
y.bera003.06 <y.bera003.06@protonmail.com>
2020-11-27 17:33:02 +0000

    Improve git mirror script

--- mirror-to-git.sh
+++ mirror-to-git.sh
@@ -4,3 +4,5 @@
 git init ../icy_matrix-git
 touch ../icy_matrix-git/git.marks
 darcs convert export --read-marks darcs.marks --write-marks darcs.marks | (cd ../icy_matrix-git && git fast-import --import-marks=git.marks --export-marks=git.marks)
+git --git-dir=../icy_matrix-git/.git --work-tree=../icy_matrix-git restore --staged
+git --git-dir=../icy_matrix-git/.git --work-tree=../icy_matrix-git restore
