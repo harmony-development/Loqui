bfe68a6bc4440fb642c05806a5d929a44b64d185
y.bera003.06 <y.bera003.06@protonmail.com>
2020-11-27 17:06:21 +0000

    Add script i use to mirror to git

---
+++ mirror-to-git.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# requirements: darcs and git
+
+git init ../icy_matrix-git
+touch ../icy_matrix-git/git.marks
+darcs convert export --read-marks darcs.marks --write-marks darcs.marks | (cd ../icy_matrix-git && git fast-import --import-marks=git.marks --export-marks=git.marks)
