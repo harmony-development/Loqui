98fb3f02c721920902a5000d442cccb652e3f81d
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-03-03 15:35:29 +0300

    feat: disable logging of most graphics stuff

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use client::content::ContentStore;
 use iced::{Application, Settings};
-use tracing::Level;
 use ui::screen::ScreenManager;
 
 pub mod client;
@@ -12,16 +11,8 @@ async fn main() {
     let content_store = ContentStore::default();
     content_store.create_req_dirs().unwrap();
 
-    let filter_level = std::env::args()
-        .nth(1)
-        .map_or(Level::INFO, |s| match s.as_str() {
-            "-v" | "--verbose" => Level::TRACE,
-            "-d" | "--debug" => Level::DEBUG,
-            _ => Level::INFO,
-        });
-
     tracing_subscriber::fmt()
-        .with_max_level(filter_level)
+        .with_env_filter("info,wgpu_core=off,iced_wgpu=off")
         .pretty()
         .init();
 
