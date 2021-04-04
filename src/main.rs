90ee1630d7bb787fdf064f4f7863cd90098dfe4b
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-04-04 18:03:29 +0300

    feat: log to file, better default logging levels

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use client::content::ContentStore;
 use ui::{screen::ScreenManager, style::DEF_SIZE};
 
 use iced::{Application, Settings};
-use tracing_subscriber::EnvFilter;
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
 pub mod client;
 pub mod ui;
@@ -15,14 +15,20 @@ async fn main() {
     let content_store = ContentStore::default();
     content_store.create_req_dirs().unwrap();
 
-    tracing_subscriber::fmt()
-        .with_env_filter(
-            EnvFilter::try_from_default_env()
-            .unwrap_or_else( |_|
-                EnvFilter::from("info,wgpu_core=off,iced_wgpu=off,gfx_memory=off,gfx_descriptor=off,gfx_backend_vulkan=off")
-            )
-        )
-        .pretty()
+    let term_logger = fmt::layer();
+    let log_file = content_store.log_file();
+    let file_appender =
+        tracing_appender::rolling::never(log_file.parent().unwrap(), log_file.file_name().unwrap());
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    let file_logger = fmt::layer().with_ansi(false).with_writer(non_blocking);
+
+    tracing_subscriber::registry()
+        .with(EnvFilter::try_from_default_env()
+        .unwrap_or_else( |_|
+            EnvFilter::from("info,wgpu_core=error,iced_wgpu=error,gfx_memory=error,gfx_descriptor=error,gfx_backend_vulkan=error")
+        ))
+        .with(term_logger)
+        .with(file_logger)
         .init();
 
     let mut settings = Settings::with_flags(content_store);
