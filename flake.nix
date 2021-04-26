7b12a0796bbc3aa305841bbf4384328ea2ee7064
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-04-27 00:11:56 +0300

    chore(deps): update flake and cargo deps

--- flake.nix
+++ flake.nix
@@ -1,19 +1,9 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    naersk = {
-      url = "github:yusdacra/naersk/feat/cargolock-git-deps";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    rustOverlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     nixCargoIntegration = {
       url = "github:yusdacra/nix-cargo-integration";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.naersk.follows = "naersk";
-      inputs.rustOverlay.follows = "rustOverlay";
     };
   };
 
@@ -28,19 +18,15 @@
           }
         ];
       };
-      build = common: prevb: {
-        gitSubmodules = true;
-        allRefs = true;
-        overrideMain = prev:
-          let o = prevb.overrideMain prev; in
-          o // {
-            nativeBuildInputs = o.nativeBuildInputs ++ (with common.pkgs; [ makeWrapper wrapGAppsHook rustfmt ]);
-            fixupPhase = with common.pkgs; ''
-              wrapProgram $out/bin/crust\
-                --set LD_LIBRARY_PATH ${lib.makeLibraryPath common.runtimeLibs}\
-                --set XDG_DATA_DIRS ${hicolor-icon-theme}/share:${gnome3.adwaita-icon-theme}/share
-            '';
-          };
+      mainBuild = common: prev: {
+        nativeBuildInputs = prev.nativeBuildInputs ++ (with common.pkgs; [ makeWrapper wrapGAppsHook ]);
+        postInstall = with common.pkgs; ''
+          if [ -f $out/bin/crust ]; then
+            wrapProgram $out/bin/crust\
+              --set LD_LIBRARY_PATH ${lib.makeLibraryPath common.runtimeLibs}\
+              --set XDG_DATA_DIRS ${hicolor-icon-theme}/share:${gnome3.adwaita-icon-theme}/share
+          fi
+        '';
       };
     };
   };
